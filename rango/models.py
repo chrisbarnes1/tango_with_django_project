ca9ec5fb84faa575d2553c72d68f102f3dbded13
Chris Barnes <2031986b@student.gla.ac.uk>
2018-01-30 18:28:56 +0000

    Chapter 5 Progress

--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self): # For python 2, use __unicode__ too
         return self.name
@@ -14,6 +19,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
 
     def __str__(self): #For Python 2, use __unicode__ too
         return self.title
