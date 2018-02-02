ae51b78bff4d5cf8774e2dadd33f09ad661fe4c5
Chris Barnes <2031986b@student.gla.ac.uk>
2018-02-02 15:14:18 +0000

    Chapter 9 exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from rango.models import Category
 from rango.models import Page
+from rango.models import UserProfile
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,3 +12,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
