78c10064b336cea23c864d4a918c595b095cfa06
Chris Barnes <2031986b@student.gla.ac.uk>
2018-02-05 14:22:02 +0000

    Back to C9

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,7 @@ from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
